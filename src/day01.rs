36989127a4ad7e4280de01655706349cef89b479
alex800121 <alex800121@hotmail.com>
2024-06-11 07:52:15 +0800

    day3

--- src/day01.rs
+++ src/day01.rs
@@ -1,10 +1,70 @@
+use itertools::Itertools;
 use project_root::get_project_root;
+use std::cmp::Ordering::*;
 
+fn solve(n: usize, target: i32, input: &[i32]) -> Option<i32> {
+    let mut i = 0;
+    let mut v: Vec<(usize, i32)> = Vec::new();
+    let mut acc = target;
+    let mut n = n;
+    loop {
+        match n.cmp(&0) {
+            Equal => match acc.cmp(&0) {
+                Equal => {
+                    return Some(v.iter().map(|x| x.1).product());
+                }
+                Less => {
+                    let (j, x) = v.pop()?;
+                    i = j + 1;
+                    n += 1;
+                    acc += x;
+                }
+                Greater => {
+                    let (_, x) = v.pop()?;
+                    acc += x;
+                    let (j, x) = v.pop()?;
+                    i = j + 1;
+                    n += 2;
+                    acc += x;
+                }
+            },
+            _ => match acc.cmp(&0) {
+                Equal | Less => {
+                    let (j, x) = v.pop()?;
+                    i = j + 1;
+                    n += 1;
+                    acc += x;
+                }
+                Greater => {
+                    if let Some(x) = input.get(i) {
+                        v.push((i, *x));
+                        acc -= x;
+                        n -= 1;
+                        i += 1;
+                    } else {
+                        let (j, x) = v.pop()?;
+                        i = j + 1;
+                        n += 1;
+                        acc += x;
+                    }
+                }
+            },
+        }
+    }
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let mut input = input
+        .lines()
+        .filter_map(|x| x.parse::<i32>().ok())
+        .collect_vec();
+    input.sort();
+    input.reverse();
+    println!("day1a: {}", solve(2, 2020, &input[..]).unwrap());
+    println!("day1b: {}", solve(3, 2020, &input[..]).unwrap());
 }
--- src/day02.rs
+++ src/day02.rs
@@ -1,10 +1,39 @@
+use itertools::Itertools;
 use project_root::get_project_root;
 
+fn valid_a((a, b, c, pass): &(u32, u32, char, &str)) -> bool {
+    let l = pass.chars().filter(|x| x == c).count() as u32;
+    l >= *a && l <= *b
+}
+fn valid_b((a, b, c, pass): &(u32, u32, char, &str)) -> bool {
+    (|| {
+        let c0 = pass.chars().nth(*a as usize - 1)?;
+        let c1 = pass.chars().nth(*b as usize - 1)?;
+        Some((c0 == *c || c1 == *c) && c0 != c1)
+    })()
+    .unwrap_or(false)
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let input = input
+        .lines()
+        .filter_map(|l| {
+            let (a, b) = l.split_once('-')?;
+            let (b, c) = b.split_once(' ')?;
+            let (c, pass) = c.split_once(": ")?;
+            Some((
+                a.parse::<u32>().ok()?,
+                b.parse::<u32>().ok()?,
+                c.chars().next()?,
+                pass,
+            ))
+        })
+        .collect_vec();
+    println!("day2a: {}", input.iter().filter(|x| valid_a(x)).count());
+    println!("day2b: {}", input.iter().filter(|x| valid_b(x)).count());
 }
