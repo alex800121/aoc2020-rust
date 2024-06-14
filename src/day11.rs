c677103743614b1bfbf458e4711c85a9c5826425
alex800121 <alex800121@hotmail.com>
2024-06-14 19:39:06 +0800

    day12

--- src/day11.rs
+++ src/day11.rs
@@ -1,10 +1,131 @@
+use std::array::from_fn;
+
+use itertools::Itertools;
+use num::ToPrimitive;
 use project_root::get_project_root;
 
+type Index = (usize, usize);
+
+#[derive(Debug, Copy, Clone, PartialOrd, PartialEq, Ord, Eq)]
+enum Space {
+    Floor,
+    Occupied,
+    Vacant,
+}
+
+const W: usize = 97;
+const H: usize = 93;
+type Seats = [[Space; W]; H];
+const ADJ: [(isize, isize); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
+fn step_a(seats: &Seats) -> Seats {
+    from_fn(|y| {
+        from_fn(|x| calc_seat(seats, (x, y), 4, |i| adjacent(seats, i)).unwrap_or(Space::Floor))
+    })
+}
+fn step_b(seats: &Seats) -> Seats {
+    from_fn(|y| from_fn(|x| calc_seat(seats, (x, y), 5, |i| far(seats, i)).unwrap_or(Space::Floor)))
+}
+fn get_seat(seats: &Seats, x: isize, y: isize) -> Option<Space> {
+    let x = x.to_usize()?;
+    let y = y.to_usize()?;
+    seats.get(y)?.get(x).copied()
+}
+fn far(seats: &Seats, i: Index) -> usize {
+    let mut output = 0;
+    for (x, y) in ADJ {
+        let (mut x0, mut y0) = (i.0 as isize + x, i.1 as isize + y);
+        while let Some(s) = get_seat(seats, x0, y0) {
+            match s {
+                Space::Occupied => {
+                    output += 1;
+                    break;
+                }
+                Space::Vacant => {
+                    break;
+                }
+                _ => {
+                    x0 += x;
+                    y0 += y;
+                }
+            }
+        }
+    }
+    output
+}
+fn calc_seat(
+    seats: &Seats,
+    (x, y): Index,
+    tolerable: usize,
+    adj_f: impl FnOnce(Index) -> usize,
+) -> Option<Space> {
+    use Space::*;
+    let adj = adj_f((x, y));
+    let c = seats.get(y)?.get(x)?;
+    Some(match c {
+        Vacant if adj == 0 => Occupied,
+        Occupied if adj >= tolerable => Vacant,
+        x => *x,
+    })
+}
+fn solve(seats: &Seats, f: impl Fn(&Seats) -> Seats) -> usize {
+    let mut seats = *seats;
+    loop {
+        let next_seats = f(&seats);
+        if seats == next_seats {
+            return seats
+                .iter()
+                .map(|l| l.iter().filter(|x| **x == Space::Occupied).count())
+                .sum::<usize>();
+        } else {
+            seats = next_seats;
+        }
+    }
+}
+fn adjacent(m: &Seats, (x, y): Index) -> usize {
+    ADJ.iter()
+        .filter(|(a, b)| {
+            let x = a + x as isize;
+            let y = b + y as isize;
+            x >= 0
+                && y >= 0
+                && m.get(y as usize)
+                    .is_some_and(|l| l.get(x as usize).is_some_and(|c| c == &Space::Occupied))
+        })
+        .count()
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let floor: Seats = input
+        .lines()
+        .filter_map(|l| {
+            l.chars()
+                .filter_map(|c| match c {
+                    '.' => Some(Space::Floor),
+                    'L' => Some(Space::Vacant),
+                    '#' => Some(Space::Occupied),
+                    _ => None,
+                })
+                .collect_vec()
+                .try_into()
+                .ok()
+        })
+        .collect_vec()
+        .try_into()
+        .unwrap();
+    println!("day11a: {}", solve(&floor, step_a));
+    println!("day11b: {}", solve(&floor, step_b));
 }
--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,92 @@
+use aoc2020::Direction::{self, *};
 use project_root::get_project_root;
 
+type Coord = (isize, isize);
+
+#[derive(Debug, PartialOrd, PartialEq, Ord, Eq, Clone, Copy)]
+struct Ferry {
+    coord: Coord,
+    waypoint: Coord,
+}
+#[derive(Debug, PartialOrd, PartialEq, Ord, Eq, Clone, Copy)]
+enum Ins {
+    D(Direction, isize),
+    F(isize),
+    T(usize),
+}
+impl Ferry {
+    fn coord_dir(&mut self, n: isize, d: Direction) {
+        let (x, y) = d.to_index();
+        self.coord = (self.coord.0 + (x * n), self.coord.1 + (y * n));
+    }
+    fn way_dir(&mut self, n: isize, d: Direction) {
+        let (x, y) = d.to_index();
+        self.waypoint = (self.waypoint.0 + (x * n), self.waypoint.1 + (y * n));
+    }
+    fn forward(&mut self, n: isize) {
+        let (x, y) = self.waypoint;
+        self.coord = (self.coord.0 + (x * n), self.coord.1 + (y * n));
+    }
+    fn way_turn(&mut self, n: usize) {
+        for _ in 0..n {
+            self.waypoint = (-self.waypoint.1, self.waypoint.0);
+        }
+    }
+}
+fn parse_ins(input: &str) -> Option<Ins> {
+    use Ins::*;
+    let (x, y) = input.split_at(1);
+    match (x, y.parse::<isize>().ok()?) {
+        ("N", n) => Some(D(North, n)),
+        ("S", n) => Some(D(South, n)),
+        ("E", n) => Some(D(East, n)),
+        ("W", n) => Some(D(West, n)),
+        ("F", n) => Some(F(n)),
+        ("R", n) => Some(T((n / 90) as usize % 4)),
+        ("L", n) => Some(T((-n / 90).rem_euclid(4) as usize)),
+        _ => None,
+    }
+}
+fn step_b(input: &str, ferry: &mut Ferry) {
+    use Ins::*;
+    match parse_ins(input) {
+        Some(D(d, n)) => ferry.way_dir(n, d),
+        Some(F(n)) => ferry.forward(n),
+        Some(T(n)) => ferry.way_turn(n),
+        _ => {}
+    }
+}
+fn step_a(input: &str, ferry: &mut Ferry) {
+    use Ins::*;
+    match parse_ins(input) {
+        Some(D(d, n)) => ferry.coord_dir(n, d),
+        Some(F(n)) => ferry.forward(n),
+        Some(T(n)) => ferry.way_turn(n),
+        _ => {}
+    }
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        // "{}/input/test{:02}.txt",
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let input_a = input.lines();
+    let mut ferry_a = Ferry {
+        coord: (0, 0),
+        waypoint: (East.to_index()),
+    };
+    let mut ferry_b = Ferry {
+        coord: (0, 0),
+        waypoint: (10, -1),
+    };
+    for input in input_a {
+        step_a(input, &mut ferry_a);
+        step_b(input, &mut ferry_b);
+        // dbg!(&ferry_a);
+    }
+    println!("day12a: {}", ferry_a.coord.0.abs() + ferry_a.coord.1.abs());
+    println!("day12b: {}", ferry_b.coord.0.abs() + ferry_b.coord.1.abs());
 }
