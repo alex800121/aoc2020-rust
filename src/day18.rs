64436421c0b060c0b63cadb8bcf7c47f89a436ba
alex800121 <alex800121@hotmail.com>
2024-06-22 07:57:54 +0800

    day18

--- src/day18.rs
+++ src/day18.rs
@@ -10,7 +10,7 @@ enum PrimToken<N> {
     Mul,
 }
 
-type Token = PrimToken<i32>;
+type Token = PrimToken<i64>;
 
 fn parse_tokens(input: &str) -> Vec<Token> {
     use PrimToken::*;
@@ -32,35 +32,195 @@ fn parse_tokens(input: &str) -> Vec<Token> {
                             input.next();
                         }
                         _ => {
-                            output.push(Value(v as i32));
                             break 'a;
                         }
                     }
                 }
+                output.push(Value(v as i64));
             }
             _ => {}
         }
     }
     output
 }
-fn solve_b(input: &[Token]) -> Option<i32> {
+fn solve_a(input: &[Token]) -> Option<i64> {
     use PrimToken::*;
-    let mut thunk = Vec::new();
+    let mut input = input.iter();
+    let mut current_token = input.next().copied();
     let mut current_thunk = Vec::new();
-    let mut input = input.iter().peekable();
-    let mut current_token = input.next();
+    let mut thunk = Vec::new();
     while let Some(t) = current_token {
         match t {
-            Open => todo!(),
-            Close => todo!(),
+            Close => {
+                let mut c = std::mem::take(&mut current_thunk);
+                let mut c = c.drain(..);
+                let mut x: i64;
+                if let Some(Value(n)) = c.next() {
+                    x = n;
+                    while let (Some(op), Some(Value(y))) = (c.next(), c.next()) {
+                        match op {
+                            Mul => {
+                                x *= y;
+                            }
+                            Add => {
+                                x += y;
+                            }
+                            _ => {
+                                return None;
+                            }
+                        }
+                    }
+                } else {
+                    return None;
+                }
+                if let Some(c) = thunk.pop() {
+                    current_thunk = c;
+                }
+                current_token = Some(Value(x));
+            }
+            Open => {
+                let c = std::mem::take(&mut current_thunk);
+                thunk.push(c);
+                current_token = input.next().copied();
+            }
             Value(v) => {
-
-            },
-            Add => todo!(),
-            Mul => todo!(),
+                match (current_thunk.pop(), current_thunk.pop()) {
+                    (None, _) => {
+                        current_thunk.push(Value(v));
+                    }
+                    (Some(Add), Some(Value(x))) => {
+                        current_thunk.push(Value(v + x));
+                    }
+                    (Some(Mul), Some(Value(x))) => {
+                        current_thunk.push(Value(v * x));
+                    }
+                    (Some(a), Some(b)) => {
+                        current_thunk.push(b);
+                        current_thunk.push(a);
+                        current_thunk.push(t);
+                    }
+                    _ => {
+                        return None;
+                    }
+                }
+                current_token = input.next().copied();
+            }
+            Add | Mul => {
+                current_thunk.push(t);
+                current_token = input.next().copied();
+            }
         }
     }
-    unimplemented!()
+    let mut c = std::mem::take(&mut current_thunk);
+    let mut c = c.drain(..);
+    let mut x: i64;
+    if let Some(Value(n)) = c.next() {
+        x = n;
+        while let (Some(op), Some(Value(y))) = (c.next(), c.next()) {
+            match op {
+                Mul => {
+                    x *= y;
+                }
+                Add => {
+                    x += y;
+                }
+                _ => {
+                    return None;
+                }
+            }
+        }
+    } else {
+        return None;
+    }
+    Some(x)
+}
+fn solve_b(input: &[Token]) -> Option<i64> {
+    use PrimToken::*;
+    let mut input = input.iter();
+    let mut current_token = input.next().copied();
+    let mut current_thunk = Vec::new();
+    let mut thunk = Vec::new();
+    while let Some(t) = current_token {
+        match t {
+            Close => {
+                let mut c = std::mem::take(&mut current_thunk);
+                let mut c = c.drain(..);
+                let mut x: i64;
+                if let Some(Value(n)) = c.next() {
+                    x = n;
+                    while let (Some(op), Some(Value(y))) = (c.next(), c.next()) {
+                        match op {
+                            Mul => {
+                                x *= y;
+                            }
+                            Add => {
+                                x += y;
+                            }
+                            _ => {
+                                return None;
+                            }
+                        }
+                    }
+                } else {
+                    return None;
+                }
+                if let Some(c) = thunk.pop() {
+                    current_thunk = c;
+                }
+                current_token = Some(Value(x));
+            }
+            Open => {
+                let c = std::mem::take(&mut current_thunk);
+                thunk.push(c);
+                current_token = input.next().copied();
+            }
+            Value(v) => {
+                match (current_thunk.pop(), current_thunk.pop()) {
+                    (None, _) => {
+                        current_thunk.push(Value(v));
+                    }
+                    (Some(Add), Some(Value(x))) => {
+                        current_thunk.push(Value(v + x));
+                    }
+                    (Some(a), Some(b)) => {
+                        current_thunk.push(b);
+                        current_thunk.push(a);
+                        current_thunk.push(t);
+                    }
+                    _ => {
+                        return None;
+                    }
+                }
+                current_token = input.next().copied();
+            }
+            Add | Mul => {
+                current_thunk.push(t);
+                current_token = input.next().copied();
+            }
+        }
+    }
+    let mut c = std::mem::take(&mut current_thunk);
+    let mut c = c.drain(..);
+    let mut x: i64;
+    if let Some(Value(n)) = c.next() {
+        x = n;
+        while let (Some(op), Some(Value(y))) = (c.next(), c.next()) {
+            match op {
+                Mul => {
+                    x *= y;
+                }
+                Add => {
+                    x += y;
+                }
+                _ => {
+                    return None;
+                }
+            }
+        }
+    } else {
+        return None;
+    }
+    Some(x)
 }
 pub fn run(day: usize) {
     let input = std::fs::read_to_string(format!(
@@ -70,5 +230,6 @@ pub fn run(day: usize) {
     ))
     .unwrap();
     let input = input.lines().map(parse_tokens).collect_vec();
-    dbg!(input);
+    println!("day18a: {}", input.iter().filter_map(|x| solve_a(x)).sum::<i64>());
+    println!("day18b: {}", input.iter().filter_map(|x| solve_b(x)).sum::<i64>());
 }
